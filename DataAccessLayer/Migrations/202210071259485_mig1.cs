fc9578b746c5cadd2e8fe0e931c7f7a1ea508072
FurkannBulut <eng.bulutt@gmail.com>
2022-10-12 14:56:44 +0300

    ilk commit

---
+++ DataAccessLayer/Migrations/202210071259485_mig1.cs
@@ -0,0 +1,22 @@
+﻿namespace DataAccessLayer.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class mig1 : DbMigration
+    {
+        public override void Up() //kaydedersek bunlar eklenecek
+        {
+            AddColumn("dbo.Contents", "WriterID", c => c.Int());
+            CreateIndex("dbo.Contents", "WriterID");
+            AddForeignKey("dbo.Contents", "WriterID", "dbo.Writers", "WriterID");
+        }
+        
+        public override void Down() //kaydetmezsek bunlar olacak
+        {
+            DropForeignKey("dbo.Contents", "WriterID", "dbo.Writers");
+            DropIndex("dbo.Contents", new[] { "WriterID" });
+            DropColumn("dbo.Contents", "WriterID");
+        }
+    }
+}
